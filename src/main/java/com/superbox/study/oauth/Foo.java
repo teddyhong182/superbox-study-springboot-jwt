41af6245b7ee456831096e9d7b11bfd220efaf1c
teddyhong182 <rusdlsla@gmail.com>
2022-05-28 15:54:05 +0900

    모델, API 추가

---
+++ src/main/java/com/superbox/study/oauth/Foo.java
@@ -0,0 +1,15 @@
+package com.superbox.study.oauth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Foo {
+
+    private long id;
+    private String name;
+
+}
---
+++ src/main/java/com/superbox/study/oauth/FooController.java
@@ -0,0 +1,33 @@
+package com.superbox.study.oauth;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/foos")
+public class FooController {
+
+    @GetMapping
+    public List<Foo> findAll() {
+        List<Foo> fooList = new ArrayList<>();
+        fooList.add(new Foo(Long.parseLong(randomNumeric(2)), randomAlphabetic(4)));
+        fooList.add(new Foo(Long.parseLong(randomNumeric(2)), randomAlphabetic(4)));
+        fooList.add(new Foo(Long.parseLong(randomNumeric(2)), randomAlphabetic(4)));
+
+        return fooList;
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void create(@RequestBody Foo newFoo) {
+        log.info("Foo created");
+    }
+}
