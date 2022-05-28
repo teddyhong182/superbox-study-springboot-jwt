4004a10ce9ded779f2ae39af7893cd48f1a4172f
teddyhong182 <rusdlsla@gmail.com>
2022-05-28 15:49:28 +0900

    first commit.

---
+++ src/main/java/com/superbox/study/oauth/SuperboxStudySpringbootOauthApplication.java
@@ -0,0 +1,14 @@
+package com.superbox.study.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableAuthorizationServer
+public class SuperboxStudySpringbootOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperboxStudySpringbootOauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/superbox/study/oauth/SuperboxStudySpringbootOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superbox.study.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperboxStudySpringbootOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
